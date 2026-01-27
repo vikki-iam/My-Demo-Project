2be859e4ca69d56120ee3dbe6e9f2c59b9aed7ff
vikki-iam <vignesh.raveendran31@gmail.com>
2026-01-27 22:23:02 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().write("Hello, Welcome to Magic Show..!");
+        resp.getWriter().write("Hello, Welcome to Magic Showhhhjhh..!");
 
         // ❌ Hardcoded credentials (Security issue)
         String password = $pass;
